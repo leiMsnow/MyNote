0718724effb0d387cf56ab979cde3c2aa6db2248
ray <always_0122@163.com>
2015-05-09 11:23:57 +0800

    add source

--- app/src/main/java/com/ray/note/MainActivity.java
+++ app/src/main/java/com/ray/note/MainActivity.java
@@ -4,14 +4,17 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
